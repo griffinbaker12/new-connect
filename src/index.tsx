d441fbd35f2fb3f975d61d5931bae7d3b33cbd9d
griffinbaker12 <96966609+griffinbaker12@users.noreply.github.com>
2024-07-12 00:31:19 -0500

    pretty much there

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,6 @@ import './index.css'
 import { render } from 'solid-js/web'
 import App from './App'
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then(registration => {
-        console.log('Service Worker registered successfully:', registration.scope)
-      })
-      .catch(error => {
-        console.log('Service Worker registration failed:', error)
-      })
-  })
-}
-
 const root = document.getElementById('root')
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
@@ -25,3 +13,10 @@ if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
 }
 
 render(() => <App />, root!)
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    console.log("Service Worker Registrations:", registrations);
+  });
+}
+
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,58 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import devtools from 'solid-devtools/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   plugins: [
     devtools(),
     solid({
       ssr: false,
+    }),
+    VitePWA({
+      strategies: 'injectManifest',
+      srcDir: '.',
+      filename: 'sw.ts',
+      manifest: {
+        short_name: 'connect',
+        name: 'comma connect',
+        id: '/',
+        start_url: '/?pwa=1',
+        background_color: '#1B1B1F',
+        display: 'standalone',
+        scope: '/',
+        theme_color: '#1B1B1F',
+        description: 'manage your openpilot experience',
+        icons: [
+          {
+            src: '/images/pwa/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/images/pwa/manifest-icon-192.maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+          {
+            src: '/images/pwa/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any',
+          },
+          {
+            src: '/images/pwa/manifest-icon-512.maskable.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
+      },
+      injectManifest: {
+        injectionPoint: undefined,
+      },
     }),
   ],
   server: {
@@ -17,7 +63,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: 'index.html',
-        sw: 'public/sw.ts',
       },
     },
   },
